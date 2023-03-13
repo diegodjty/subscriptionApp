214f71c4420524d79dd9b8333ad9744fd6ae6330
Diego <diegotaveras2@outlook.com>
2023-03-13 14:41:19 -0400

    Create DemoScreen

--- App.tsx
+++ App.tsx
@@ -3,8 +3,15 @@ import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
+import DemoScreen from "./screens/DemoScreen";
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  Home: undefined;
+  Paywall: undefined;
+  Demo: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
@@ -16,6 +23,13 @@ export default function App() {
           }}
           name="Home"
           component={HomeScreen}
+        />
+        <Stack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Demo"
+          component={DemoScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- components/ActionRow.tsx
+++ components/ActionRow.tsx
@@ -1,5 +1,13 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../App";
+
+export type NavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  "Home"
+>;
 
 type Props = {
   title: string;
@@ -11,8 +19,10 @@ type Props = {
 };
 
 const ActionRow = ({ title, screen, color, requiresPro, icon, vertical }) => {
+  const navigation = useNavigation<NavigationProp>();
   return (
     <TouchableOpacity
+      onPress={() => navigation.navigate(screen)}
       className={`flex m-2 flex-1 justify-center items-center p-5 py-6 rounded-lg space-x-2 ${
         vertical ? "flex-col" : "flex-row"
       }`}
