9537b36fe5e646c597acaeae1c5bd49f30625b3e
Diego <diegotaveras2@outlook.com>
2023-03-13 15:18:17 -0400

    create PaywallScreen screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -8,11 +8,23 @@ import {
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import ActionRow from "../components/ActionRow";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../App";
+import { useNavigation } from "@react-navigation/native";
+
+export type NavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  "Paywall"
+>;
 const HomeScreen = () => {
+  const navigation = useNavigation<NavigationProp>();
   return (
     <SafeAreaView className="flex 1 bg-gray-100 relative">
       <ScrollView>
-        <TouchableOpacity className="absolute z-50 top-5 right-10 items-center">
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Paywall")}
+          className="absolute z-50 top-5 right-10 items-center"
+        >
           <Ionicons name="person-circle" size={30} color="#E5962D" />
           <Text className="text-center text-[#E5962D]">UPGRADE</Text>
         </TouchableOpacity>
